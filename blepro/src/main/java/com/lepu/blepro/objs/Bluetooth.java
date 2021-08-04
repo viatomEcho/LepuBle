84e7eb5e209ea9a93aef116cf6e33f6f6e6a16d7
viatomEcho <wujuan@viatomtech.com>
2021-08-04 19:42:34 +0800

    bp2a

--- blepro/src/main/java/com/lepu/blepro/objs/Bluetooth.java
+++ blepro/src/main/java/com/lepu/blepro/objs/Bluetooth.java
@@ -112,7 +112,7 @@ public class Bluetooth implements Parcelable {
             case BT_NAME_BP2:
                 return MODEL_BP2;
             case BT_NAME_BP2A:
-                return MODEL_BP2A;
+                return MODEL_BP2;
             case BT_NAME_RINGO2:
                 return MODEL_RINGO2;
             case BT_NAME_O2MAX:
