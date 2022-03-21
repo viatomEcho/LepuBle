d118d9f204b88ef2d8a51cbfc4b727200b84790e
chenyongfeng <chenyongfeng@viatomtech.com>
2022-03-21 15:13:05 +0800

    文件crc返回值类型

--- blepro/src/main/java/com/lepu/blepro/ble/data/FileListCrc.kt
+++ blepro/src/main/java/com/lepu/blepro/ble/data/FileListCrc.kt
@@ -1,14 +1,14 @@
 package com.lepu.blepro.ble.data
 
-import com.lepu.blepro.utils.bytesToHex
+import com.lepu.blepro.utils.toUInt
 
 class FileListCrc {
     var fileType: Int  // 文件类型 LeBp2wBleCmd.FileType.ECG_TYPE, BP_TYPE, USER_TYPE,
-    var crc: String    // 0表示文件不存在
+    var crc: Int       // 0表示文件不存在
 
     constructor(fileType: Int, crc: ByteArray) {
         this.fileType = fileType
-        this.crc = bytesToHex(crc)
+        this.crc = toUInt(crc)
     }
 
     override fun toString(): String {
