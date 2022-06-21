a2415722957c0c2a11f2b9ed88ecfdbe3932280f
chenyongfeng <chenyongfeng@viatom.com>
2022-06-21 15:24:28 +0800

    蓝牙名一致sdk不做处理 2.0.2.2-reconnect

--- blepro/src/main/java/com/lepu/blepro/ble/service/BleService.kt
+++ blepro/src/main/java/com/lepu/blepro/ble/service/BleService.kt
@@ -778,7 +778,7 @@ open class BleService: LifecycleService() {
 
                 if (isReconnectScan && isContains){
                     stopDiscover()
-                    if (isReconnectByAddress) {
+//                    if (isReconnectByAddress) {
                         // 避免体温计aoj20a在装电池开机后自动关机过程连接上设备，延迟重连
                         if (model == Bluetooth.MODEL_AOJ20A) {
                             GlobalScope.launch {
@@ -790,14 +790,14 @@ open class BleService: LifecycleService() {
                             vailFace.get(b.model)?.connect(this@BleService, b.device, true, toConnectUpdater)
                             LepuBleLog.d(tag, "发现需要重连的设备....去连接 model = ${b.model} name = ${b.name}  address = ${b.macAddr}")
                         }
-                    } else {
-                        if (BleServiceHelper.BleServiceHelper.canReconnectByName(b.model)) {
-                            vailFace.get(b.model)?.connect(this@BleService, b.device, true, toConnectUpdater)
-                            LepuBleLog.d(tag, "发现需要重连的设备....去连接 model = ${b.model} name = ${b.name}  address = ${b.macAddr}")
-                        } else {
-                            LepuBleLog.d(tag, "发现需要重连的设备不可使用蓝牙名重连 model = ${b.model} name = ${b.name}  address = ${b.macAddr}")
-                        }
-                    }
+//                    } else {
+//                        if (BleServiceHelper.BleServiceHelper.canReconnectByName(b.model)) {
+//                            vailFace.get(b.model)?.connect(this@BleService, b.device, true, toConnectUpdater)
+//                            LepuBleLog.d(tag, "发现需要重连的设备....去连接 model = ${b.model} name = ${b.name}  address = ${b.macAddr}")
+//                        } else {
+//                            LepuBleLog.d(tag, "发现需要重连的设备不可使用蓝牙名重连 model = ${b.model} name = ${b.name}  address = ${b.macAddr}")
+//                        }
+//                    }
 
                 } else {
                     if (isReconnectScan) {
