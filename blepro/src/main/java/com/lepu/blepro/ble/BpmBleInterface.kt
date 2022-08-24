358a9cde114a30870c89a2022d44a3fd614559f6
chenyongfeng <chenyongfeng@viatom.com>
2022-08-24 10:04:34 +0800

    合并问题

--- blepro/src/main/java/com/lepu/blepro/ble/BpmBleInterface.kt
+++ blepro/src/main/java/com/lepu/blepro/ble/BpmBleInterface.kt
@@ -96,7 +96,7 @@ class BpmBleInterface(model: Int): BleInterface(model) {
         when(BpmBleCmd.getMsgType(bytes)) {
             BpmBleCmd.BPMCmd.MSG_TYPE_GET_INFO -> {
                 //设备信息
-                val deviceInfo = BpmDeviceInfo(bytes, bluetooth.name)
+                val data = BpmDeviceInfo(bytes, bluetooth.name)
 
                 LepuBleLog.d(tag, "model:$model,GET_INFO => success")
 
